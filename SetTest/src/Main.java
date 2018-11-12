5f3ae0a2232a07c81ccca3180c5fa3a25b9f11e9
Pugachev Eugene <eugene.pugachev@gmail.com>
2018-11-12 23:00:24 +0300

    add line

--- SetTest/src/Main.java
+++ SetTest/src/Main.java
@@ -8,6 +8,7 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
+
         Set<String> words = new HashSet<>();
 // объект типа HashSet, реализующий хеш-множество
         long totalTime = 0;
